3ff3245d14fc7db688e44eb547f8e8c98692c22b
Ayyappa <ayyappasrikakulapu@gmail.com>
2025-03-17 17:16:32 +0530

    Add files via upload

---
+++ calculator project.py
@@ -0,0 +1,47 @@
+
+while(True):
+    print("-"*50)
+    print("\t\t\t\tCALCULATOR")
+    print("-"*50)
+    # """Means multi line quotes
+    print("""\t\t1.ADDITION
+    \t2.SUBSCRATION
+    \t3.MULTIPLICATION
+    \t4.DIVISION
+    \t5.EXPONENTATION
+    \t6.EXIT""" )
+    print("-"*50)
+    try:
+      ch=int(input("ENTER ANY ONE ABOVE THIS OPTIONS:"))
+      match(ch):
+                    case 1:
+                        print("Enter Two Values for Addition")
+                        print("-" * 50)
+                        a, b = float(input("Enter first Value:")), float(input(("Enter Second Value:")))
+                        print("\tSUM({},{})={}".format(a, b, a + b))
+                    case 2:
+                        print("Enter Two number for SUBSCRATION")
+                        print("-" * 50)
+                        a, b = float(input("Enter first Value:")), float(input("Enter Second Value:"))
+                        print("\tSUB({},{})={}".format(a, b, a - b))
+                    case 3:
+                        print("Enter Two number for MULTIPLICATION")
+                        print("-" * 50)
+                        a, b = float(input("Enter first Value :")), float(input("Enter Second Value:"))
+                        print("\tMUL({},{})={}".format(a, b, a * b))
+                    case 4:
+                        print("Enter Two Values for DIVISION")
+                        print("-" * 50)
+                        a, b = float(input("Enter first Value:")), float(input("Enter Second Value:"))
+                        print("\tDIV({},{})={}".format(a, b, a / b))
+                    case 5:
+                        print("Enter Two Values for.EXPONENTATION")
+                        print("-" * 50)
+                        a, b = float(input("Enter Base value:")), float(input("Enter Power Value:"))
+                        print("Pow({},{})={}".format(a, b, a ** b))
+                    case 6:
+                        print("Thanks for using this Program\n We will meet Next Time")
+                    case _:
+                        print("Don't Enter Invalid Number plz try--again")
+    except ValueError:
+        print("Don't Enter space & special symbols  plz---> try again!")
