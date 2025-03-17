b57c33ce30dbf69070815b5f65d9091d5fce9e3b
Ayyappa <ayyappasrikakulapu@gmail.com>
2025-03-17 17:30:15 +0530

    Delete addition.py

--- addition.py
+++
@@ -1,7 +0,0 @@
-def Addition():
-    print("Enter Two Values for Addition")
-    print("-" * 50)
-    a, b = float(input("Enter first Value:")), float(input(("Enter Second Value:")))
-    print("\tSUM({},{})={}".format(a, b, a + b))
-
-Addition()
